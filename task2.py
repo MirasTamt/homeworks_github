a3a5b54a9352b4d5d89c3826fa0bf7a882179965
Miras <miras.tamtiyev@ya.ru>
2020-06-19 18:40:34 +0300

    delete old

--- task2.py
+++
@@ -1,4 +0,0 @@
-import random
-initial_list =  [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-my_list = [initial_list[i] for i in range(1,len(initial_list),1) if initial_list[i] > initial_list[i-1]]
-print(my_list)
--- task7.py
+++
@@ -1,8 +0,0 @@
-from math import factorial
-
-def fact(n):
-    for num in range(1, n + 1):
-        yield factorial(num)
-
-for el in fact(int(input('Input number: '))):
-    print(el)
